ee066e12a46f0cd5406a26d4729dffc9b1e43942
Artur Wiebe <artur@4wiebe.de>
2019-10-04 11:05:49 +0200

    gui: teach graph

---
+++ root/projectroot/usr/lib/gui/gui.js
@@ -0,0 +1,59 @@
+class GraphXY extends Chart {
+	constructor() {
+		let canvas = document.createElement("canvas");
+		canvas.width = canvas.height = 100;
+		super(canvas, {
+			type: 'line',
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'linear',
+						position: 'bottom',
+						ticks: {
+							stepSize: 10,
+							suggestedMin: -200,
+							suggestedMax:  200
+						},
+						scaleLabel: {display:false}
+					}],
+					yAxes: [{
+						type: 'linear',
+						position: 'left',
+						ticks: {
+							stepSize: 10,
+							suggestedMin: -200,
+							suggestedMax:  200
+						},
+						scaleLabel: {display:false}
+					}]
+				}
+			},
+			data: {datasets: [{
+				backgroundColor: 'green',
+				borderColor:     'green',
+				pointRadius:     10,
+				data: [{x:0, y:0}]
+			}]}
+		});
+	}
+	
+	updatePos(x, y) {
+		this.data.datasets[0].data[0] = {x:x, y:y};
+		this.update();
+	}
+}
+
+
+Chart.defaults.global.legend.display		= false;
+Chart.defaults.global.animation				= 0;
+Chart.defaults.global.tooltips.enabled		= false;
+Chart.defaults.global.elements.line.tension	= 0;
+
+
+
+
+
+ko.bindingHandlers.element = {update: function(element, valueAccessor) {
+	$(element).empty();
+	$(element).append(ko.utils.unwrapObservable(valueAccessor()));
+}};
