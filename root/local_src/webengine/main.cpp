f0d164b1df3256bc16816e49f1c00d503068c7d8
Artur Wiebe <artur@4wiebe.de>
2020-01-23 15:56:19 +0100

    webview: basic support

--- root/local_src/webengine/main.cpp
+++ root/local_src/webengine/main.cpp
@@ -4,15 +4,15 @@
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setOrganizationName("QtExamples");
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-    
-    QGuiApplication app(argc, argv);
-    QtWebEngine::initialize();
+	QCoreApplication::setOrganizationName("QtExamples");
+	QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+	QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+	QGuiApplication app(argc, argv);
+	QtWebEngine::initialize();
 
-    return app.exec();
+	QQmlApplicationEngine engine;
+	engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+	return app.exec();
 }
