8a86e03e0ad4af98e4ef7c57afe16c95b2f5fe39
Artur Wiebe <artur@4wiebe.de>
2020-05-13 16:19:09 +0200

    VarioShaker

---
+++ root/rules/app.make
@@ -0,0 +1,24 @@
+#
+# We provide this package
+#
+PACKAGES-$(PTXCONF_APP) += app
+
+APP_VERSION		:= 1
+
+# ----------------------------------------------------------------------------
+# Target-Install
+# ----------------------------------------------------------------------------
+
+$(STATEDIR)/app.targetinstall:
+	@$(call targetinfo)
+	@$(call install_init, app)
+
+	@$(call install_alternative, app, 0, 0, 0755, /usr/sbin/shaker)
+	@$(call install_alternative, app, 0, 0, 0644, /usr/lib/systemd/system/shaker.service)
+	@$(call install_link,        app, ../shaker.service, /usr/lib/systemd/system/mc@shaker.target.wants/shaker.service)
+
+	@$(call install_finish,app)
+	@$(call touch)
+
+
+# vim: syntax=make
--- vision.py
+++ vision.py
@@ -15,10 +15,11 @@ cap.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 960)
 cap.set(cv.CAP_PROP_SHARPNESS, 0)
 cap.set(cv.CAP_PROP_AUTOFOCUS, 0)
-cap.set(cv.CAP_PROP_FOCUS, 0)
+cap.set(cv.CAP_PROP_FOCUS, 60)
 cap.set(cv.CAP_PROP_AUTO_EXPOSURE, 1)
-cap.set(cv.CAP_PROP_EXPOSURE, 30)
+cap.set(cv.CAP_PROP_EXPOSURE, 3)
 
+print("ready to capture")
 
 
 
@@ -27,9 +28,9 @@ def getRaw():
 	return cv.cvtColor(frame, cv.COLOR_YUV2GRAY_YUYV)
 
 def getImage():
-	homography = np.array([[2.83307306e+00, -1.55534698e-01, -3.19332024e+02],[6.07683410e-03, 2.73114832e+00, -9.71618468e+01],[5.79265182e-06, -1.04055238e-04, 1.00000000e+00]])
+	homography = np.array([[-2.64775813e+00, -1.03469375e-01, 2.66602673e+03],[1.18218397e-02, -2.77722517e+00, 2.13563180e+03],[3.31794082e-06, -1.02626229e-04, 1.00000000e+00]])
 	img = getRaw()
-	return cv.warpPerspective(img, homography, (3000,2000))
+	return cv.warpPerspective(img, homography, (1750,1750))
 
 
 def show(img):
@@ -41,25 +42,25 @@ def show(img):
 
 
 def calibrateHomography():
-	points = np.mgrid[0:9, 0:7].T.reshape(-1, 2) * 200 + [1500-4*200, 1000-3*200]
+	points = np.mgrid[0:7, 0:5].T.reshape(-1, 2) * 200 + [950-3*200, 650-2*200]
 	
 	gray = getRaw()
 	
-	ret,corners = cv.findChessboardCorners(gray, (9,7))
+	ret,corners = cv.findChessboardCorners(gray, (7,5))
 	corners = cv.cornerSubPix(gray, corners, (10,10), (-1,-1), (cv.TERM_CRITERIA_EPS+cv.TERM_CRITERIA_COUNT, 30, 0.01))
 	
 	homography,status = cv.findHomography(corners, points)
 	
-	cv.drawChessboardCorners(gray, (9,7), corners, ret)
+	cv.drawChessboardCorners(gray, (7,5), corners, ret)
 	show(gray)
-	img = cv.warpPerspective(gray, homography, (3000,2000))
+	img = cv.warpPerspective(gray, homography, (1750,1750))
 	show(img)
 	
 	return homography
 
 
 def robotPoint(imageX, imageY):
-	return (+imageY/10-100-50, -imageX/10+150)
+	return (-imageY/10+65, imageX/10-95)
 
 
 
@@ -67,17 +68,15 @@ def findObjects():
 	gray = getImage()
 	
 	img = cv.GaussianBlur(gray, (25, 25), 0)
-	ret,img = cv.threshold(img, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+	ret,img = cv.threshold(img, 0, 255, cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
 	
 	contours,h = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 	
 	for c in contours:
-		M = cv.moments(c)
-		cX = int(M["m10"] / M["m00"])
-		cY = int(M["m01"] / M["m00"])
-		print(robotPoint(cX, cY))
+		(cX,cY),r = cv.minEnclosingCircle(c)
+		print(robotPoint(cX, cY), r)
 		
-		cv.circle(gray, (cX, cY), 7, (255, 255, 255), -1)
+		cv.circle(gray, (int(cX), int(cY)), 7, (255, 255, 255), -1)
 
 
 	cv.drawContours(gray, contours, -1, (0, 255, 0), 2)
@@ -85,8 +84,9 @@ def findObjects():
 
 
 
+#show(getRaw())
 #print(calibrateHomography())
-#show(getImage())
+show(getImage())
 findObjects()
 
 cap.release()
