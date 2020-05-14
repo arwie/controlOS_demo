2e602ffa1ecc64a7a43a7dd08f47ba5b3811385c
Artur Wiebe <artur@4wiebe.de>
2020-05-14 18:08:36 +0200

    VarioShaker improved

--- vision.py
+++ vision.py
@@ -8,6 +8,7 @@ import time
 
 cap = cv.VideoCapture('/dev/video0', cv.CAP_V4L2)
 
+cap.set(cv.CAP_PROP_BUFFERSIZE, 1)
 cap.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc('Y','U','Y','V'))
 cap.set(cv.CAP_PROP_CONVERT_RGB, 0)
 cap.set(cv.CAP_PROP_FORMAT, -1)
@@ -23,14 +24,21 @@ print("ready to capture")
 
 
 
+homography = np.array([[-2.77567915e+00, -1.00764105e-01, 2.69350242e+03],[1.03750041e-02, -2.88806990e+00, 1.89610723e+03],[7.45911550e-06, -1.08313725e-04, 1.00000000e+00]])
+
+def robotPoint(imageX, imageY):
+	return (-imageY/10+85-50, imageX/10-90)
+
+
+
 def getRaw():
+	cap.grab()	#clear buffer
 	ret,frame = cap.read()
 	return cv.cvtColor(frame, cv.COLOR_YUV2GRAY_YUYV)
 
 def getImage():
-	homography = np.array([[-2.64775813e+00, -1.03469375e-01, 2.66602673e+03],[1.18218397e-02, -2.77722517e+00, 2.13563180e+03],[3.31794082e-06, -1.02626229e-04, 1.00000000e+00]])
 	img = getRaw()
-	return cv.warpPerspective(img, homography, (1750,1750))
+	return cv.warpPerspective(img, homography, (1800,1800))
 
 
 def show(img):
@@ -40,9 +48,8 @@ def show(img):
 	cv.waitKey(0)
 
 
-
 def calibrateHomography():
-	points = np.mgrid[0:7, 0:5].T.reshape(-1, 2) * 200 + [950-3*200, 650-2*200]
+	points = np.mgrid[0:7, 0:5].T.reshape(-1, 2) * 200 + [900-3*200, 850-2*200]
 	
 	gray = getRaw()
 	
@@ -53,15 +60,10 @@ def calibrateHomography():
 	
 	cv.drawChessboardCorners(gray, (7,5), corners, ret)
 	show(gray)
-	img = cv.warpPerspective(gray, homography, (1750,1750))
+	img = cv.warpPerspective(gray, homography, (1800,1800))
 	show(img)
 	
 	return homography
-
-
-def robotPoint(imageX, imageY):
-	return (-imageY/10+65, imageX/10-95)
-
 
 
 def findObjects():
@@ -73,10 +75,11 @@ def findObjects():
 	contours,h = cv.findContours(img, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 	
 	for c in contours:
-		(cX,cY),r = cv.minEnclosingCircle(c)
-		print(robotPoint(cX, cY), r)
+		(x,y),r	= cv.minEnclosingCircle(c)
+		a		= cv.contourArea(c)
+		print(robotPoint(x, y), r, a)
 		
-		cv.circle(gray, (int(cX), int(cY)), 7, (255, 255, 255), -1)
+		cv.circle(gray, (int(x), int(y)), 7, (255, 255, 255), -1)
 
 
 	cv.drawContours(gray, contours, -1, (0, 255, 0), 2)
@@ -86,7 +89,7 @@ def findObjects():
 
 #show(getRaw())
 #print(calibrateHomography())
-show(getImage())
+#show(getImage())
 findObjects()
 
 cap.release()
