6142aa4568bf9767c0be04479db20e7783c5cbd9
Artur Wiebe <artur@4wiebe.de>
2020-12-01 19:18:48 +0100

    joystick: wirking quite fine

--- root/local_src/arduino/joystick/joystick.ino
+++ root/local_src/arduino/joystick/joystick.ino
@@ -12,9 +12,12 @@
 #define analogDamping	60
 
 
-float xCenter, yCenter;
-
 WebsocketsClientJson wsClient;
+
+float xCenter, yCenter;
+float a, b, c;
+float x, y;
+bool gripped;
 
 
 
@@ -22,72 +25,164 @@ void setup() {
 	pinMode(LED_BUILTIN, OUTPUT);
 	digitalWrite(LED_BUILTIN, HIGH);
 	
-	while (!Serial) { ; }
+	for (float a,b,c=1.0; !Serial && c>0.5; IMU.readAcceleration(a,b,c)) { IMU.begin(); }
 	
 	syswlan_begin(IPAddress(90,0,0,31));
 	update(version);
-	
-	IMU.begin();
 	
 	joystickCenter();
 	Serial.print(xCenter); Serial.print(" # "); Serial.println(yCenter);
 	
 	wsClient.begin("90.0.0.1", 55001);
+	wsClient.onMessageJson<256>(handleInfo);
 	
 	digitalWrite(LED_BUILTIN, LOW);
 }
 
 
 
-void loop() {
-	static bool idle = true;
-	//calibrateJoystick();	return;
-	//calibrateIMU();		return;
+void sendCmd(JsonDocument& data, const int cmd = 0) {
+	data["cmd"] = cmd;
+	wsClient.sendJson(data);
+	digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void handleInfo(JsonDocument& data)
+{
+	gripped = data["gripped"];
+}
+
+
+bool controlDelta(JsonDocument& data) {
+	static int tilted = 0;
 	
-	auto x = +joystickDirection(analogReadFiltered<xInput, analogDamping>(), 465, xCenter, 727);
-	auto y = -joystickDirection(analogReadFiltered<yInput, analogDamping>(), 342, yCenter, 685);
-	Serial.print(x); Serial.print(" / "); Serial.println(y);
-	
-	StaticJsonDocument<256> data;
-	
-	if (abs(x)>0 || abs(y)>0) {
-		data["abs"]			= false;
-		data["dir"]["x"]	= 0;
-		data["dir"]["y"]	= 0;
-		data["dir"]["z"]	= 0;
-		data["dir"]["r"]	= 0;
-		
-		if (abs(x)>abs(y)) {
-			data["dir"]["x"]	= x>0 ? 1 : -1;
-			data["speed"]		= 10*abs(x);
-		} else {
-			data["dir"]["y"]	= y>0 ? 1 : -1;
-			data["speed"]		= 10*abs(y);
-		}
-		
-		sendCmd(data, 1);
-		idle = false;
+	if (c < -0.7) {
+		tilted = 0;
 	} else {
-		if (!idle) {
-			sendCmd(data, 0);
-			idle = true;
+		int tilt = abs(a)>abs(b) ? 1: -1;
+		if (!tilted) {
+			tilted = tilt;
+		}
+		static bool gripSent = false;
+		if (tilted!=tilt) {
+			if (!gripSent) {
+				data["grip"] = !gripped;
+				sendCmd(data, -2);
+				gripSent = true;
+			}
+		} else {
+			gripSent = false;
 		}
 	}
 	
+	if (x==0 && y==0)
+		return true;
+	
+	data["abs"]			= false;
+	data["dir"]["x"]	= 0;
+	data["dir"]["y"]	= 0;
+	data["dir"]["z"]	= 0;
+	data["dir"]["r"]	= 0;
+	
+	switch (tilted) {
+		case 0:
+			if (abs(x)>abs(y)) {
+				data["dir"]["x"]	= x>0 ? 1 : -1;
+				data["speed"]		= 10*abs(x);
+			} else {
+				data["dir"]["y"]	= y>0 ? 1 : -1;
+				data["speed"]		= 10*abs(y);
+			}
+			break;
+		case 1:
+			data["dir"]["z"]	= (a<0 && x<0) || (a>0 && x>0) ? 1 : -1;
+			data["speed"]		= 10*abs(x);
+			break;
+		case -1:
+			data["dir"]["z"]	= (b<0 && y<0) || (b>0 && y>0) ? 1 : -1;
+			data["speed"]		= 10*abs(y);
+			break;
+	}
+	
+	sendCmd(data, 1);
+	return false;
+}
+
+
+bool controlConv(JsonDocument& data) {
+	if (y==0)
+		return true;
+	
+	data["speed"]		= 10*abs(y);
+	data["dir"]["x"]	= y<0 ? 1 : -1;
+	data["dir"]["y"]	= 0;
+	data["dir"]["z"]	= 0;
+	data["dir"]["r"]	= 0;
+	
+	sendCmd(data, 11);
+	return false;
+}
+
+
+
+void loop() {
+	//calibrateJoystick();	return;
+	//calibrateIMU();		return;
+	
+	x = +joystickDirection(analogReadFiltered<xInput, analogDamping>(), 465, xCenter, 727);
+	y = -joystickDirection(analogReadFiltered<yInput, analogDamping>(), 342, yCenter, 685);
+	//Serial.print(x); Serial.print(" / "); Serial.println(y);
+	
+	if (x==0 && y==0) {
+		IMU.begin();
+		IMU.readAcceleration(a, b, c);
+		//Serial.print(a); Serial.print(" / "); Serial.print(b); Serial.print(" / "); Serial.println(c);
+	}
+	
+	StaticJsonDocument<256> data;
+	static int select = 1;
+	bool stop = true;
+	
+	if (select) {
+		if (c > 0.8) {
+			select = 0;
+			Serial.print("Select: ");
+		}
+	} else {
+		if (a < -0.8) {
+			select = 1;
+			Serial.println("DELTA");
+		}
+		if (a > +0.8) {
+			select = 11;
+			Serial.println("CONVEYOR");
+		}
+	}
+	
+	switch(select) {
+		case 1:
+			stop = controlDelta(data);
+			break;
+		case 11:
+			stop = controlConv(data);
+			break;
+	}
+	
+	static bool stopped = true;
+	if (stop) {
+		if (!stopped) {
+			sendCmd(data);
+			stopped = true;
+		}
+	} else {
+		stopped = false;
+	}
 	
 	wsClient.loop();
 	delay(25);
 	digitalWrite(LED_BUILTIN, LOW);
 	delay(25);
 }
-
-
-void sendCmd(JsonDocument& data, const int cmd) {
-	data["cmd"] = cmd;
-	wsClient.sendJson(data);
-	digitalWrite(LED_BUILTIN, HIGH);
-}
-
 
 
 void joystickCenter() {
@@ -140,10 +235,11 @@ void calibrateJoystick() {
 
 
 void calibrateIMU() {
-	float x, y, z;
+	float a, b, c;
 	
-	IMU.readAcceleration(x, y, z);
+	IMU.begin();
+	IMU.readAcceleration(a, b, c);
+	Serial.print(a); Serial.print(" / "); Serial.print(b); Serial.print(" / "); Serial.println(c);
 	
-	Serial.print(x); Serial.print(" / "); Serial.print(y); Serial.print(" / "); Serial.println(z);
 	delay(500);
 }
