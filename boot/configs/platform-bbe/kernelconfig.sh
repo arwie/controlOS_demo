2ecd6415b8df906fb8093869c675d13fb693fb82
Artur Wiebe <artur@4wiebe.de>
2023-11-09 12:08:38 +0100

    updated platform-bbe

--- boot/configs/platform-bbe/kernelconfig.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $(realpath $0))
-
-cd $BASEDIR
-
-
-cat bb.org_defconfig ../kernelconfig-common.in kernelconfig.in > kernelconfig
--- root/configs/platform-bbe/kernelconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $(realpath $0))
-
-cd $BASEDIR
-
-BASECONFIGS="../../base/configs"
-
-cat $BASECONFIGS/platform-bbe/bb.org_defconfig $BASECONFIGS/kernelconfig-common.in $BASECONFIGS/platform-bbe/kernelconfig-platform.in kernelconfig.in > kernelconfig
