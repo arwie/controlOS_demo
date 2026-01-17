43cf55169e7a1482316de890b88915a0dd2cb5dc
Artur Wiebe <artur@4wiebe.de>
2026-01-17 15:35:18 +0100

    app: assigned buttons io

--- code/app/buttons.py
+++ code/app/buttons.py
@@ -1,11 +1,29 @@
 from shared import app
+from shared.app import codesys
 
 
 
-@app.input
+@app.input(sim=True)
 def start() -> bool:
-	return False
+	return codesys.fbk.io[1]
 
 @app.input
 def stop() -> bool:
-	return False
+	return codesys.fbk.io[2]
+
+@app.output
+def led_running(value:bool):
+	codesys.cmd.io[1] = value
+
+
+@app.input
+def left() -> bool:
+	return codesys.fbk.io[3]
+
+@app.input
+def right() -> bool:
+	return codesys.fbk.io[4]
+
+@app.output
+def led_arrows(value:bool):
+	codesys.cmd.io[2] = value
